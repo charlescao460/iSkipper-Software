b35ebb77240c53c6ce087a8a82d8a170f8cbe5cd
charlescao460 <charlescao@csr460.com>
2018-12-27 16:37:48 -0600

    Support JavaFX Maven build

--- Software/iSkipper-JavaFX/src/application/Main.java
+++ Software/iSkipper-JavaFX/src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import com.jfoenix.controls.JFXButton;
 
 public class Main extends Application
 {
