0b4ad5703b948535d34e9924053b66580ce89cd7
charlescao460 <charlescao@csr460.com>
2019-01-01 03:00:33 -0600

    Add User Preferences support

---
+++ Software/iSkipper-JavaFX/src/application/utils/preference/SavedID.java
@@ -0,0 +1,181 @@
+package application.utils.preference;
+
+import java.util.ArrayList;
+
+import support.IClickerID;
+
+/**
+ * The class to storage ID related information on the GUI.
+ * 
+ * @author CSR
+ *
+ */
+public class SavedID
+{
+	private IClickerID id;
+	private String name;
+	private String note;
+
+	/**
+	 * Default Constructor
+	 */
+	public SavedID()
+	{
+	}
+
+	/**
+	 * @param id
+	 * @param name
+	 * @param note
+	 */
+	public SavedID(IClickerID id, String name, String note)
+	{
+		if (id == null)
+			throw new NullPointerException("ID Cannot be Null!");
+		this.id = id;
+		this.name = name;
+		this.note = note;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public IClickerID getId()
+	{
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(IClickerID id)
+	{
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the note
+	 */
+	public String getNote()
+	{
+		return note;
+	}
+
+	/**
+	 * @param note
+	 *            the note to set
+	 */
+	public void setNote(String note)
+	{
+		this.note = note;
+	}
+
+	/**
+	 * The ArrayList to storage saved IDs.
+	 * 
+	 * @author CSR
+	 *
+	 */
+	public static class SavedIDList extends ArrayList<SavedID>
+	{
+		private static final long serialVersionUID = -1764507006360279163L;
+
+		/**
+		 * Return the saved IDs in CSV string..
+		 * 
+		 * @see java.util.AbstractCollection#toString()
+		 */
+		@Override
+		public String toString()
+		{
+			StringBuilder builder = new StringBuilder();
+			for (SavedID id : this)
+			{
+				builder.append(id.getId().toString());
+				builder.append(',');
+				builder.append(id.getName());
+				builder.append(',');
+				builder.append(id.getNote());
+				builder.append(',');
+				builder.append('\n');
+			}
+			return builder.toString();
+		}
+
+		/**
+		 * @return The length of the CSV string.
+		 */
+		public long csvLength()
+		{
+			long ret = 0;
+			for (SavedID id : this)
+			{
+				ret += 8; // Hexadecimal id length
+				if (id.getName() != null)
+					ret += id.getName().length();
+				else
+					ret += 4; // "null"
+				if (id.getNote() != null)
+					ret += id.getNote().length();
+				else
+					ret += 4;
+				ret += 4; // Commas and '\n'
+			}
+			return ret;
+		}
+
+		/**
+		 * Construct a list from CSV string.
+		 * 
+		 * @param csv
+		 *            The CSV string containing the saved IDs
+		 * @throws @{@link
+		 * 			IndexOutOfBoundsException} If incorrect format.
+		 * 
+		 * @throws @{@link
+		 * 			NullPointerException} If there is an invalid ID.
+		 */
+		SavedIDList(String csv)
+		{
+			String strIDs[] = csv.split("\n");
+			for (String strID : strIDs)
+			{
+				String info[] = strID.split(",");
+				IClickerID id = IClickerID.idFromString(info[0]);
+				if (id == null)
+					throw new NullPointerException();
+				String name = info[1].equals("null") ? null : info[1];
+				String note = info[2].equals("null") ? null : info[2];
+				this.add(new SavedID(id, name, note));
+			}
+		}
+
+		/**
+		 * Default constructor.
+		 */
+		public SavedIDList()
+		{
+			super();
+		}
+
+	}
+
+}
---
+++ Software/iSkipper-JavaFX/src/application/utils/preference/UserPreferences.java
@@ -0,0 +1,180 @@
+/**
+ * 
+ */
+package application.utils.preference;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+import application.utils.preference.SavedID.SavedIDList;
+import support.IClickerChannel;
+
+/**
+ * The Class to storage user's settings and preferences. (E.g. Channel, IDs)
+ * 
+ * @author CSR
+ *
+ */
+public final class UserPreferences
+{
+	private static Preferences preferences;
+	private static IClickerChannel channel;
+	private final static String KEY_CHANNEL = "KEY_CHANNEL";
+	private static String listCSVLocation;
+	private final static String PREFERENCE_FOLDER_NAME = "iSkipper";
+	private final static String CSV_FILE_NAME = "iSkipperIDs.csv";
+	private static SavedID.SavedIDList savedIDList;
+
+	static
+	{
+		readPreferences();
+	}
+
+	private static void readPreferences()
+	{
+		preferences = Preferences.userNodeForPackage(UserPreferences.class);
+		channel = readChannel();
+		listCSVLocation = readCSVFilePath();
+		savedIDList = readList();
+	}
+
+	private static SavedIDList readList()
+	{
+		File csvFile = new File(listCSVLocation);
+		try
+		{
+			String strCSV = new String(Files.readAllBytes(csvFile.toPath()));
+			return new SavedID.SavedIDList(strCSV);
+
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return new SavedID.SavedIDList();
+	}
+
+	private static String readCSVFilePath()
+	{
+		String strUserHome = System.getenv("user.home");
+		File folder = new File(strUserHome, PREFERENCE_FOLDER_NAME);
+		if (!folder.exists())
+		{// Then we create it
+			folder.mkdir();
+		}
+		File csvFile = new File(folder, CSV_FILE_NAME);
+		if (!csvFile.exists())
+		{
+			try
+			{
+				csvFile.createNewFile();
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return csvFile.toString();
+	}
+
+	private static IClickerChannel readChannel()
+	{
+		try
+		{
+			// Read channel
+			String strChannel = preferences.get(KEY_CHANNEL, null);
+			if (strChannel == null)
+			{
+				// If no previous record
+				preferences.put(KEY_CHANNEL, IClickerChannel.AA.toString());
+				return IClickerChannel.AA;
+			} else
+			{
+				return IClickerChannel.valueOf(strChannel);
+			}
+
+		} catch (Exception e)
+		{
+			// Then the record is corrupted。
+			e.printStackTrace();
+			try
+			{
+				preferences.clear();// So, we trash it.
+				setAllToDefault();
+			} catch (BackingStoreException e1)
+			{
+				e1.printStackTrace();
+			}
+		}
+		return IClickerChannel.AA;
+	}
+
+	private static void setAllToDefault()
+	{
+		String strUserHome = System.getenv("user.home");
+		File folder = new File(strUserHome, PREFERENCE_FOLDER_NAME);
+		folder.mkdir();
+		File csvFile = new File(folder, CSV_FILE_NAME);
+		csvFile.delete();
+		try
+		{
+			csvFile.createNewFile();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @return the savedIDList
+	 */
+	public static SavedID.SavedIDList getSavedIDList()
+	{
+		return savedIDList;
+	}
+
+	/**
+	 * @param savedIDList
+	 *            the savedIDList to set
+	 */
+	public static void setSavedIDList(SavedID.SavedIDList savedIDList)
+	{
+		if (savedIDList == null)
+			throw new NullPointerException("savedIDList cannot set to be null!");
+		UserPreferences.savedIDList = savedIDList;
+
+		try
+		{
+			FileWriter writer = new FileWriter(listCSVLocation, false);
+			writer.write(UserPreferences.savedIDList.toString());// Overwrite
+			writer.close();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * @return the channel
+	 */
+	public static IClickerChannel getChannel()
+	{
+		return channel;
+	}
+
+	/**
+	 * @param channel
+	 *            the channel to set
+	 */
+	public static void setChannel(IClickerChannel channel)
+	{
+		if (channel == null)
+			throw new NullPointerException("Channel cannot set to be null!");
+		UserPreferences.channel = channel;
+		preferences.put(KEY_CHANNEL, UserPreferences.channel.toString());
+	}
+
+}
